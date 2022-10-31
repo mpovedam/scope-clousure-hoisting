7ca02b0fa67dc8ddd1ade412c57eb24aecb0da53
Mauricio Poveda <mauriciomacias2.0@gmail.com>
2022-10-30 22:03:05 -0500

    Hoisting

---
+++ scr/Hoisting/hoisting.js
@@ -0,0 +1,25 @@
+/* console.log(nameOfDog);
+var nameOfDog = 'Elmo';
+console.log(nameOfDog); */
+
+
+
+
+nameOfDog();
+
+function nameOfDog()
+{
+    console.log(`El mejor perrito es ${elmo}`);
+}
+
+var elmo = 'Elmito';
+
+//se eleva la variable elmo y queda inicializada con undefined
+//la función tambien se eleva en tiempo de ejecución y luego se ejecuta
+
+/* var elmo;
+function nameOfDog()
+{
+    console.log(`El mejor perrito es ${elmo}`);
+}
+nameOfDog(); */
